c91368ad099094f15a118f882e11728c31681a35
heanthien <hthien.pbh@gmail.com>
2018-09-24 23:18:34 +0700

    me

--- resources/views/include/other-income-agriculture.blade.php
+++ resources/views/include/other-income-agriculture.blade.php
@@ -118,7 +118,7 @@
                     <td><b style="float: right;"> 7. C កុមារ(អាយុក្រោម ១៨ឆ្នាំ )រកចំណូល</b> </td>
                     <td>
                         <div class="input-group">
-                            <input class="cal_child cal_age form-control" id="income_child_score"  type="text" name="income_child_score">
+                            <input class="cal_child cal_age form-control" id="income_child_score"  type="text" name="income_child_score" value="8">
                             <span class="input-group-addon">ពិន្ទុ</span>
                         </div>
                     </td>
@@ -138,15 +138,15 @@
     </li>
 </ul>
 <script>
-    $('.cal_age').change(function(){
-        var my_id = $('.myrow').attr('index');
-        var small_age = $('.cal_child_'+my_id).val();
-           $('.txt_age').each(function(i){
-               var age = $(this).val();
-               if(i > 0 && (parseFloat(age) == 10)) small_age = age;
-           });
-            $('#income_child_score').val(small_age);
-    });
+    // $('.cal_age').change(function(){
+    //     var my_id = $('.myrow').attr('index');
+    //     var small_age = $('.cal_child_'+my_id).val();
+    //        $('.txt_age').each(function(i){
+    //            var age = $(this).val();
+    //            if(i > 0 && (parseFloat(age) == 10)) small_age = age;
+    //        });
+    //         $('#income_child_score').val(small_age);
+    // });
     $('.income_agriculture').click(function () {
         var index = $(this).attr('index');
         // console.log(index);
