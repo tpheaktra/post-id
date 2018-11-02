acb7a1af3b07970dd50fc410f038c7d727cdaff5
tpheaktra.pbh <tpheaktra.pbh@gmail.com>
2018-11-02 15:33:52 +0700

    updated contact

--- resources/views/contact/contact-index.blade.php
+++ resources/views/contact/contact-index.blade.php
@@ -7,7 +7,116 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="user-mangement">
-                    <center>Comming Soon...</center>
+                    <h3 class="hospital_title" align="center">សម្រាប់ព័ត៌មានបន្ថែមសូមទំនាក់ទំនង</h3>
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                            <td colspan="7" style="border-top: none;"><big class="text-primary">ក្រសួងសុខាភិបាល-នាយកដ្ឋានផែនការ និងព័ត៌មានសុខាភិបាល (ការិយាល័យ សេដ្ឋកិច្ច និងហិរញ្ញប្បទានសុខាភិបាល)</big></td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">1.</td>
+                            <td style="border-top: none;">លោកវេជ្ជបណ្ឌិត ឡូ វាសនាគីរី</td>
+                            <td colspan="5" style="border-top: none;">ប្រធាននាយកដ្ឋានផែនការ និងព័ត៌មានសុខាភិបាល</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">2.</td>
+                            <td style="border-top: none;">លោកស្រីវេជ្ជបណ្ឌិត សុខ កញ្ញា</td>
+                            <td style="border-top: none;">អនុប្រធាននាយកដ្ឋានផែនការ និងព័ត៌មានសុខាភិបាល</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 12 622 107</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">3.</td>
+                            <td style="border-top: none;">លោកវេជ្ជបណ្ឌិត ប៊ុន សំណាង</td>
+                            <td style="border-top: none;">ប្រធានការិយាល័យសេដ្ឋកិច្ច និងហិរញ្ញប្បទានសុខាភិបាល      </td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 12 926 058</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">4.</td>
+                            <td style="border-top: none;">លោកស្រី ថោ បូនី </td>
+                            <td style="border-top: none;">អនុប្រធានការិយាល័យសេដ្ឋកិច្ច និងហិរញ្ញប្បទានសុខាភិបាល</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 12 937 340</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">5.</td>
+                            <td style="border-top: none;">លោក ចាប សាត </td>
+                            <td style="border-top: none;">អនុប្រធានការិយាល័យសេដ្ឋកិច្ច និងហិរញ្ញប្បទានសុខាភិបាល</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 12 528 809</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">5.</td>
+                            <td style="border-top: none;">លោក ភូ សុភាព </td>
+                            <td style="border-top: none;">មន្រ្តីការិយាល័យសេដ្ឋកិច្ច និងហិរញ្ញប្បទានសុខាភិបាល</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 77 737 833</td>
+                        </tr>
+
+
+                        <tr>
+                            <td colspan="7" style="border-top: none;"><big class="text-primary">គម្រោងគាំពារសុខភាពសង្គម</big></td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">&nbsp;</td>
+                            <td colspan="6" rowspan="1" style="border-top: none;"><big class="text-primary">.:: ផ្នែកជំនួយបច្ចេកទេសទាក់ទិននិងជំងឺក្នុងមន្ទីរពេទ្យ</big></td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">1.</td>
+                            <td style="border-top: none;">វេជ្ជ. សំ អេង</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 12 617 225</td>
+                            <td style="border-top: none;">Email : seng@urc-chs.com</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">2.</td>
+                            <td style="border-top: none;">វេជ្ជ. ឡាវ ចន្ថា</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 16 937 841</td>
+                            <td style="border-top: none;">Email : lchantha@urc-chs.com</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">3.</td>
+                            <td style="border-top: none;">វេជ្ជ. ពុធ ពៅសុជាតិ</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 86 668 858 / 077 668 658</td>
+                            <td style="border-top: none;">Email : ppousocheat@urc-chs.com</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">4.</td>
+                            <td style="border-top: none;">លោក លុត ម៉ាឌី</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 78 786 767</td>
+                            <td style="border-top: none;">Email : lmardy@urc-chs.com</td>
+                        </tr>
+                        <tr>
+                            <td style="border-top: none;">5.</td>
+                            <td style="border-top: none;">វេជ្ជ. សែម កល្យាណ</td>
+                            <td colspan="4" style="border-top: none;">Tel : (855) 017 779 907</td>
+                            <td style="border-top: none;"></td>
+                        </tr>
+                        {{--<tr>--}}
+                            {{--<td style="border-top: none;">&nbsp;</td>--}}
+                            {{--<td colspan="6" rowspan="1" style="border-top: none;"><big class="text-primary">.:: ផ្នែកជំនួយបច្ចេកទេសទាក់ទងនិងពត៌មានវិទ្យា&nbsp;(IT)</big></td>--}}
+                        {{--</tr>--}}
+                        {{--<tr>--}}
+                            {{--<td style="border-top: none;">1.</td>--}}
+                            {{--<td style="border-top: none;">លោក ហុង រ័ត្ន</td>--}}
+                            {{--<td colspan="4" style="border-top: none;">Tel : (855) 11 820 908</td>--}}
+                            {{--<td style="border-top: none;">Email : hroith@pbhcam.org</td>--}}
+                        {{--</tr>--}}
+                        {{--<tr>--}}
+                            {{--<td style="border-top: none;">2.</td>--}}
+                            {{--<td style="border-top: none;">លោក ប៉ែន&nbsp;ដារិទ្ធ</td>--}}
+                            {{--<td colspan="4" style="border-top: none;">Tel : (855) 11 959 599</td>--}}
+                            {{--<td style="border-top: none;">Email : pdarith@pbhcam.org</td>--}}
+                        {{--</tr>--}}
+                        {{--<tr>--}}
+                            {{--<td style="border-top: none;">3.</td>--}}
+                            {{--<td style="border-top: none;">លោក ធូ&nbsp;ប៊ុនហាន់</td>--}}
+                            {{--<td colspan="4" style="border-top: none;">Tel : (855) 12 268 255</td>--}}
+                            {{--<td style="border-top: none;">Email : bunhann@open.org.kh</td>--}}
+                        {{--</tr>--}}
+                        {{--<tr>--}}
+                            {{--<td style="border-top: none;">4.</td>--}}
+                            {{--<td style="border-top: none;">លោក ធី ប៊ុនរិទ្ធ</td>--}}
+                            {{--<td colspan="4" style="border-top: none;">Tel : (855) 10 502 606</td>--}}
+                            {{--<td style="border-top: none;">Email : tbunrith@pbhcam.org</td>--}}
+                        {{--</tr>--}}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
